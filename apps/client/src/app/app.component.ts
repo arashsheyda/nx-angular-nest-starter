7f3dad15b5de0422ac48b91e54e9d9488b0ac929
arashsheyda <sheidaeearash1999@gmail.com>
2023-02-07 12:34:49 +0300

    customized welcome component

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,19 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'nx-angular-nest-starter-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  title = 'client';
-
-  constructor(private readonly http: HttpClient) {}
-
-  ngOnInit(): void {
-    this.http.get('http://localhost:9000/api').subscribe((res) => {
-      console.log(res);
-    });
-  }
-}
+export class AppComponent {}
--- apps/client/src/app/nx-welcome.component.ts
+++ apps/client/src/app/nx-welcome.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 /* eslint-disable */
 
@@ -30,9 +31,11 @@ import { Component, ViewEncapsulation } from '@angular/core';
       }
       h1,
       h2,
-      p,
       pre {
         margin: 0;
+      }
+      p {
+        margin: 10px;
       }
       *,
       ::before,
@@ -113,16 +116,22 @@ import { Component, ViewEncapsulation } from '@angular/core';
       }
       #hero {
         align-items: center;
-        background-color: hsla(214, 62%, 21%, 1);
+        background-color: #fff;
         border: none;
         box-sizing: border-box;
         color: rgba(55, 65, 81, 1);
         display: grid;
         grid-template-columns: 1fr;
         margin-top: 3.5rem;
+        box-shadow: 0 0 0 5px #dd0031;
+        transition: all 0.2s ease-in-out;
+      }
+      #hero:hover {
+        box-shadow: 0 0 0 2px #e0234e;
+      transition: all 0.2s ease-in-out;
       }
       #hero .text-container {
-        color: rgba(255, 255, 255, 1);
+        color: #000000;
         padding: 3rem 2rem;
       }
       #hero .text-container h2 {
@@ -142,11 +151,10 @@ import { Component, ViewEncapsulation } from '@angular/core';
         margin-left: 2.5rem;
       }
       #hero .text-container a {
-        background-color: rgba(255, 255, 255, 1);
+        background: linear-gradient(to left, #dd0031, #e0234e);
+        color: #ffffff;
         border-radius: 0.75rem;
-        color: rgba(55, 65, 81, 1);
         display: inline-block;
-        margin-top: 1.5rem;
         padding: 1rem 2rem;
         text-decoration: inherit;
       }
@@ -155,6 +163,9 @@ import { Component, ViewEncapsulation } from '@angular/core';
         justify-content: center;
         padding-left: 2rem;
         padding-right: 2rem;
+      }
+      #hero .logo-container img {
+        height: 127px;
       }
       #hero .logo-container svg {
         color: rgba(255, 255, 255, 1);
@@ -433,7 +444,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome client 👋
+            Welcome {{ title }} 👋
           </h1>
         </div>
 
@@ -456,19 +467,13 @@ import { Component, ViewEncapsulation } from '@angular/core';
               </svg>
               <span>You&apos;re up and running</span>
             </h2>
+            <p>
+              a message from server: <strong>{{ data }}</strong>
+            </p>
             <a href="#commands"> What&apos;s next? </a>
           </div>
           <div class="logo-container">
-            <svg
-              fill="currentColor"
-              role="img"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M11.987 14.138l-3.132 4.923-5.193-8.427-.012 8.822H0V4.544h3.691l5.247 8.833.005-3.998 3.044 4.759zm.601-5.761c.024-.048 0-3.784.008-3.833h-3.65c.002.059-.005 3.776-.003 3.833h3.645zm5.634 4.134a2.061 2.061 0 0 0-1.969 1.336 1.963 1.963 0 0 1 2.343-.739c.396.161.917.422 1.33.283a2.1 2.1 0 0 0-1.704-.88zm3.39 1.061c-.375-.13-.8-.277-1.109-.681-.06-.08-.116-.17-.176-.265a2.143 2.143 0 0 0-.533-.642c-.294-.216-.68-.322-1.18-.322a2.482 2.482 0 0 0-2.294 1.536 2.325 2.325 0 0 1 4.002.388.75.75 0 0 0 .836.334c.493-.105.46.36 1.203.518v-.133c-.003-.446-.246-.55-.75-.733zm2.024 1.266a.723.723 0 0 0 .347-.638c-.01-2.957-2.41-5.487-5.37-5.487a5.364 5.364 0 0 0-4.487 2.418c-.01-.026-1.522-2.39-1.538-2.418H8.943l3.463 5.423-3.379 5.32h3.54l1.54-2.366 1.568 2.366h3.541l-3.21-5.052a.7.7 0 0 1-.084-.32 2.69 2.69 0 0 1 2.69-2.691h.001c1.488 0 1.736.89 2.057 1.308.634.826 1.9.464 1.9 1.541a.707.707 0 0 0 1.066.596zm.35.133c-.173.372-.56.338-.755.639-.176.271.114.412.114.412s.337.156.538-.311c.104-.231.14-.488.103-.74z"
-              />
-            </svg>
+            <img src="/assets/nx-angular-nest.png" alt="nx angular nest">
           </div>
         </div>
 
@@ -843,4 +848,15 @@ nx affected:e2e</pre>
   styles: [],
   encapsulation: ViewEncapsulation.None,
 })
-export class NxWelcomeComponent {}
+export class NxWelcomeComponent implements OnInit {
+  title = 'client';
+  data: any;
+
+  constructor(private readonly http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:9000/api').subscribe((res: any) => {
+      this.data = res.message;
+    });
+  }
+}
